d3072a655d8246ec59163bc6646981b3b6940ed1
shintsal <70944299+shintsal@users.noreply.github.com>
2021-01-25 11:56:20 +0200

    lisää komponentit ja tallenna kuvat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    ProductListComponent,
+    ShippingComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
