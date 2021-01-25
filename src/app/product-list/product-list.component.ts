87e041d594bd140fd837567eb8339543b4f76e7a
shintsal <70944299+shintsal@users.noreply.github.com>
2021-01-25 12:48:52 +0200

    lisää tiedot product-list komponenttiin

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  products = products;
+
   constructor() { }
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 
   ngOnInit(): void {
   }
