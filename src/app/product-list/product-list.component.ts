0462e2983fe391af04cc0bd1291ac977dbc658ad
shintsal <70944299+shintsal@users.noreply.github.com>
2021-01-25 14:15:08 +0200

    muokkaa product.ts ja product-list komponentti

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-list',
@@ -9,8 +10,10 @@ import { products } from '../products';
 export class ProductListComponent implements OnInit {
 
   products = products;
+  items;
+  product;
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   share() {
     window.alert('The product has been shared!');
@@ -18,6 +21,11 @@ export class ProductListComponent implements OnInit {
 
   onNotify() {
     window.alert('You will be notified when the product goes on sale');
+  }
+
+  addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
   }
 
   ngOnInit(): void {
