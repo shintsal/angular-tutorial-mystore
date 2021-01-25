3fe86ebc4182fca6f54664c18a49069afe67e195
shintsal <70944299+shintsal@users.noreply.github.com>
2021-01-25 13:13:01 +0200

    lisää sisältö back-button directive ja backbtn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
@@ -28,6 +29,7 @@ import { BackbtnComponent } from './backbtn/backbtn.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
--- src/app/backbtn/backbtn.component.ts
+++ src/app/backbtn/backbtn.component.ts
@@ -11,5 +11,4 @@ export class BackbtnComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
